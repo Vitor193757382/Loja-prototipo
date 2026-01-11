05995852f114f8a02fe286f0b5344b63374642a8
Vitor <vitormanoeldebonadesousa@gmail.com>
2026-01-10 23:07:57 -0300

    Update product image URLs in main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-produtos = [("https://pixabay.com/vectors/blouse-cloth-fashion-kids-1133696/", "blusa", "uma blusa legal pra vestir"), ("https://pixabay.com/vectors/blouse-cloth-fashion-kids-1133696/", "blusa", "uma blusa legal pra vestir"), ("https://pixabay.com/vectors/blouse-cloth-fashion-kids-1133696/", "blusa", "uma blusa legal pra vestir")]
+produtos = [("https://cdn.pixabay.com/photo/2016/01/11/14/40/blouse-1133696_1280.png", "blusa", "uma blusa legal pra vestir"), ("https://cdn.pixabay.com/photo/2016/01/11/14/40/blouse-1133696_1280.png", "blusa", "uma blusa legal pra vestir"),("https://cdn.pixabay.com/photo/2016/01/11/14/40/blouse-1133696_1280.png", "blusa", "uma blusa legal pra vestir")]
 
 @app.route("/produtos")
 def produtosloja():
