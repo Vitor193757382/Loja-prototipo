03e323fb1a8f3720a8654b2ebeb1e7c3815362fe
Vitor <vitormanoeldebonadesousa@gmail.com>
2026-01-11 01:31:02 +0000

    main.py adicionado

---
+++ main.py
@@ -0,0 +1,12 @@
+from Flask import flask, render_template
+
+app = Flask(__name__)
+
+produtos = [()()()]
+
+@app.route("/produtos")
+def produtos()
+  return render_template("produtos.html", produtos=produtos)
+
+  if __name__ == "__main__":
+    app.run()
