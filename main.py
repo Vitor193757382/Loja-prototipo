959fdf9d2bf068726e4e03bed2e23b01f1042dcf
Vitor <vitormanoeldebonadesousa@gmail.com>
2026-01-10 23:01:43 -0300

    Update produtos list and rename produtos function

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-produtos = [()()()]
+produtos = [("https://pixabay.com/vectors/blouse-cloth-fashion-kids-1133696/", "blusa", "uma blusa legal pra vestir"), ("https://pixabay.com/vectors/blouse-cloth-fashion-kids-1133696/", "blusa", "uma blusa legal pra vestir"), ("https://pixabay.com/vectors/blouse-cloth-fashion-kids-1133696/", "blusa", "uma blusa legal pra vestir")]
 
 @app.route("/produtos")
-def produtos():
+def produtosloja():
   return render_template("produtos.html", produtos=produtos)
 
 if __name__ == "__main__":
